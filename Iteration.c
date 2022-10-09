e4c5709a4671eb400afdb7ef9b1e17af42024070
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-10-09 18:14:55 +0530

    stop

---
+++ Iteration.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+#define f(x) cos(x)-3*x+1
+#define g(x) (1+cos(x))/3
+
+int main()
+{
+    int step=1,N;
+    float x0,x1,e;
+
+    printf("Enter initial guess:");
+    scanf("%f",&x0);
+    printf("Enter tolerable error:");
+    scanf("%f",&e);
+    printf("Enter maximum iteration:");
+    scanf("%d",&N);
+
+    do
+    {
+        
+    }
+}
